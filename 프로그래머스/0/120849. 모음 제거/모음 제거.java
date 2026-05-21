c2550766a93be9e9dc0ba2971847c8e4d88dc9e2
Min-Seok Jeong <pokmeby@gmail.com>
2026-05-21 12:14:00 +0900

    Delete 프로그래머스/0 directory

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++
@@ -1,7 +0,0 @@
-class Solution {
-    public String solution(String my_string) {
-        String answer = my_string.replaceAll("a","").replaceAll("e","").replaceAll("o","").replaceAll("i","").replaceAll("u","");
-        
-        return answer;
-    }
-}
