9311efa559ce723f227cf116d499840f6532ea87
Min-Seok Jeong <pokmeby@gmail.com>
2026-05-26 23:10:57 +0900

    [level 1] Title: [1차] 비밀지도, Time: 1.54 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -2,13 +2,15 @@ class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
         
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++) {
             answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
-            answer[i] = answer[i].replace('0',' ');
-            answer[i] = answer[i].replace('1','#');
+            answer[i] = answer[i].replace("0"," ").replace("1","#");
             
-            while(answer[i].length() < n){
-                answer[i] = ' ' + answer[i];
+            //첫단어가 " " 경우 지워지기 때문에 경우 처리 
+            if(answer[i].length() < n) {
+                for(int j = 0; j <= n -answer[i].length(); j++) {
+                    answer[i] = ' ' + answer[i];    
+                }
             }
         }
         
