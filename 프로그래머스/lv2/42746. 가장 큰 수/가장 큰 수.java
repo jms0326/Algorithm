2f6c839dd9a891b7b238c84318efb048636a37bc
Min-Seok Jeong <pokmeby@gmail.com>
2026-06-04 16:08:10 +0900

    [level 2] Title: 가장 큰 수, Time: 1519.82 ms, Memory: 379 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,22 +1,26 @@
 import java.util.*;
-
-public class Solution {
+class Solution {
     public String solution(int[] numbers) {
         String answer = "";
-        String[] numberStr = new String[numbers.length];
-
-        for(int i=0;i<numbers.length;i++)
-            numberStr[i] = String.valueOf(numbers[i]);
-
-        Arrays.sort(numberStr, (s1, s2) -> (s2+s1).compareTo(s1+s2));
-
-        for(String str: numberStr)
-            answer += str;
-
-        if(answer.charAt(0) == '0'){
-            answer = "0";
+        // 문자열 배열로 변환
+        String[] number = new String[numbers.length];
+        for(int i=0;i<numbers.length;i++){
+            number[i] = String.valueOf(numbers[i]);
         }
-
+        
+        // 내림차순 정렬
+        Arrays.sort(number, new Comparator<String>(){
+            public int compare(String a, String b){
+                return (b+a).compareTo(a+b);
+            }
+        });
+        
+        // 배열에 0 만 있는 경우(정렬배열 맨처음이 0) "0" 리턴
+        if(number[0].equals("0")) return "0";
+        
+        // 문자열 합치기
+        for(String n : number)
+            answer+=n;
         return answer;
     }
 }
